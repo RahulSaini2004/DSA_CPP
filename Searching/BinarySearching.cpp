16142aff2561be7665f2b167a23ba336d2ebe1fb
rahul saini <sainisab454501072004@gmail.com>
2024-05-09 11:08:12 +0530

    Arr

--- Searching/BinarySearching.cpp
+++ Searching/BinarySearching.cpp
@@ -29,7 +29,7 @@ int binarySearch(int arr[],int size,int target){
 int main(){
     int arr[] = {1,2,3,4,5,6,7,8,9,10};
     int size = 10;
-    int target = 65;
+    int target = 5;
 
     int indexValue = binarySearch(arr,size,target);
 
---
+++ revosen/first.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    if(22==18){
+        cout<<"Match";
+
+    }
+    else{
+        cout<<"No Match";
+    }
+
+    int a = 22;
+    int b = 18;
+    if(a==b){
+        cout<<"Match";
+    }else{
+        cout<<"No Match";
+    }
+
+    int arr []= {0,1,2,3,4,5,6};
+    for(int i=0; i<6; i++) {
+        cout<<arr[i]<<endl;
+    }
+
+    int arr[] = {0, 1, 2, 3, 4, 5, 6};
+
+    for (int i = 6; i > 0; i--) {
+        cout << arr[i] << endl;
+    }
+
+    int arr[] = {1,2,3,4,5};
+        int size = sizeof(arr) / sizeof(arr[0]); 
+
+        cout << "Size of the array: " << size <<endl;
+
+        cout << "first index: " << arr[0] <<endl;
+
+        cout << "last index: " << arr[size - 1] <<endl;
+
+        return 0;
+
+
+
+
+    
+    }
