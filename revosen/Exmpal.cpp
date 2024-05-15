3d664d26a86393c72ba272cf61e386a002e5e97c
rahul saini <sainisab454501072004@gmail.com>
2024-05-15 14:08:02 +0530

    soart arr

--- revosen/Exmpal.cpp
+++ revosen/Exmpal.cpp
@@ -1,24 +1,35 @@
+// #include <bits/stdc++.h>
+//     using namespace std;
+//     int main(){
+//         vector <int> arr = {4,10,8,11,1};
+//         for (int i = 0; i < arr.size(); i++)
+//         {
+//             for (int j = i+1; j < arr.size(); j++)
+//             {
+//                 if (arr[i]>arr[j])
+//                 {
+//                    int temp  = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < arr.size(); i++)
+//         {
+//             cout<<arr[i]<<" ";
+//         }
+// } 
+
 #include <bits/stdc++.h>
-    using namespace std;
-    int main(){
-        vector <int> arr = {4,10,8,11,1};
-        for (int i = 0; i < arr.size(); i++)
-        {
-            for (int j = i+1; j < arr.size(); j++)
-            {
-                if (arr[i]>arr[j])
-                {
-                   int temp  = arr[i];
-                   arr[i] = arr[j];
-                   arr[j] = temp;
-                }
-                
-            }
-            
-        }
-        for (int i = 0; i < arr.size(); i++)
-        {
-            cout<<arr[i]<<" ";
-        }
-      
-}
+using namespace std;
+int main(){
+     vector <int> arr = {4,10,8,11,1};
+     sort(arr.begin(),arr.end());
+     for (int i = 0; i < arr.size(); i++)
+     {
+         cout<<arr[i]<<" ";
+     }
+     cout<<endl;
+        
+};
+
